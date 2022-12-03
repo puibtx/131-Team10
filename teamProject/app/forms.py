181ecac6d56b2b2d4f442cbde1e967560ea18d4d
Jose Cortes <67614349+torta19@users.noreply.github.com>
2022-12-03 14:09:41 -0800

    fixed validators and added error list for forms in signup html

--- teamProject/app/forms.py
+++ teamProject/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, BooleanField, SubmitField, StringField
-from wtforms.validators import DataRequired, Length, InputRequired, EqualTo
+from wtforms.validators import ValidationError, DataRequired, EqualTo, Length
 
 
 class LoginForm(FlaskForm):
@@ -11,14 +11,17 @@ class LoginForm(FlaskForm):
 
 
 class SignupForm(FlaskForm):
-    email = EmailField('Email', validators=[InputRequired()])
-    firstName = StringField(
-        'First Name', validators=[InputRequired(), Length(min=2, max=15)])
-    lastName = StringField('Last Name', validators=[
-                           InputRequired(), Length(min=2, max=15)])
-    username = StringField('Username', validators=[
-                           InputRequired(), Length(min=6, max=15)])
-    password = PasswordField('Password', validators=[InputRequired()])
-    confirm = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo(
-        password, message='Passwords must match')])
+    email = EmailField('Email', validators=[DataRequired()])
+    # firstName = StringField(
+    # 'First Name', validators=[DataRequired(), Length(min=2, max=15)])
+    # lastName = StringField(
+    # 'Last Name', validators=[
+    # DataRequired(), Length(min=2, max=15)])
+    username = StringField('Username', validators=[DataRequired(), Length(
+        min=6, message='Username must be at least %(min)d')])
+    password = PasswordField('Password', validators=[DataRequired(), Length(
+        min=6, message='Password must be at least %(min)d')])
+    confirm = PasswordField('Confirm Password', validators=[DataRequired(
+        message='please confirm password'),
+        EqualTo('password', message='Both password fields must be equal!')])
     create = SubmitField('Create Account')
--- teamProject/app/models.py
+++ teamProject/app/models.py
@@ -1,4 +1,4 @@
-from app import db
+from . import db
 from flask_login import UserMixin
 
 
