3e4950a9b35c4ca2e397c5be3f9b970c18692951
Jose Cortes <67614349+torta19@users.noreply.github.com>
2022-12-05 12:58:59 -0800

    login route updated to verify if user exists

--- teamProject/app/__init__.py
+++ teamProject/app/__init__.py
@@ -8,20 +8,20 @@ from flask_login import LoginManager
 #database is created
 db = SQLAlchemy()
 
-DB_NAME = "database.db"
+DB_NAME = "app.db"
 
 
 def build_app():
     myapp = Flask(__name__)
     myapp.config["SECRET_KEY"] = 'TEAM-10-ROCKS-XD'
-    from .routes import routes
+    from .routes import views
     from .auth import auth
 
     # configured database
     myapp.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     # extend the application with its contents
-    myapp.register_blueprint(routes, url_prefix='/')
+    myapp.register_blueprint(views, url_prefix='/')
     myapp.register_blueprint(auth, url_prefix='/')
 
     from .models import User
--- teamProject/app/routes.py
+++ teamProject/app/routes.py
@@ -1,24 +1,28 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
-from . import login_manager
-from flask_login import login_required, current_user, login_session
+from flask_login import login_required, current_user
 from .models import User
 from . import db
 
-routes = Blueprint('routes', __name__)
+views = Blueprint('routes', __name__)
 
 
-@routes.route('/delete/<int:id>')
+@views.route('/delete/<int:id>')
 @login_required
 def delete(id):
 
     delete_user = User.query.get_or_404(current_user.id)
-    username = login_session['username']
 
     try:
         db.session.delete(delete_user)
         db.session.commit()
-        flash(f'{username} deleted successfully')
+        flash('account deleted')
         redirect(url_for('auth.login'))
 
     except:
         flash('failed to delete account')
+
+
+@views.route('/home')
+@login_required
+def user_home():
+    return render_template('home.html')
