7143eedecc628dd05d8a07c8b4546b46d938cc90
Nick Faylor <nicholas.faylor@sjsu.edu>
2022-12-04 17:46:53 -0800

    fixed merge

--- teamProject/app/auth.py
+++ teamProject/app/auth.py
@@ -9,11 +9,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 auth = Blueprint('auth', __name__)
 
 
-<<<<<<< HEAD
-@auth.route("/")
-=======
 @auth.route("/", methods=['GET', 'POST'])
->>>>>>> 84109d3c4398770d8d53c3d9f93dcc300b7326e1
 @auth.route("/signin", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -31,19 +27,6 @@ def signup():
         #last_name = request.form.get('lastName')
         username = request.form.get('username')
         password = request.form.get('password')
-<<<<<<< HEAD
-        confirm = request.form.get('confirm')
-        if password != confirm:
-            flash("Passwords don't match")
-        else:
-            new_user = User(email=email, first_name=first_name, last_name=last_name,
-                            username=username, password=generate_password_hash(password, method='sha512'))
-            db.session.add(new_user)
-            db.session.commit()
-
-            flash('Success! welcome')
-            return redirect(url_for('auth.login'))
-=======
 
         new_user = User(email=email,
                         username=username, password=generate_password_hash(password, method='sha512'))
@@ -52,5 +35,4 @@ def signup():
 
         flash('Success! welcome')
         return redirect(url_for('auth.login'))
->>>>>>> 84109d3c4398770d8d53c3d9f93dcc300b7326e1
     return render_template("signup.html", form=form)
--- teamProject/app/forms.py
+++ teamProject/app/forms.py
@@ -1,11 +1,6 @@
 from flask_wtf import FlaskForm
-<<<<<<< HEAD
-from wtforms import EmailField, PasswordField, BooleanField, SubmitField, StringField, validators
-from wtforms.validators import DataRequired, Length, InputRequired, EqualTo
-=======
 from wtforms import EmailField, PasswordField, BooleanField, SubmitField, StringField
 from wtforms.validators import ValidationError, DataRequired, EqualTo, Length
->>>>>>> 84109d3c4398770d8d53c3d9f93dcc300b7326e1
 
 
 class LoginForm(FlaskForm):
@@ -16,17 +11,6 @@ class LoginForm(FlaskForm):
 
 
 class SignupForm(FlaskForm):
-<<<<<<< HEAD
-    email = EmailField('Email', validators=[InputRequired()])
-    firstName = StringField(
-        'First Name', validators=[InputRequired(), Length(min=2, max=15)])
-    lastName = StringField('Last Name', validators=[
-                           InputRequired(), Length(min=2, max=15)])
-    username = StringField('Username', validators=[
-                           InputRequired(), Length(min=6, max=15)])
-    password = PasswordField('Password', validators = [DataRequired(), Length(min=8)], id = "password")
-    confirm = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password', message = "Passwords don't match")], id = "conpassword")
-=======
     email = EmailField('Email', validators=[DataRequired()])
     # firstName = StringField(
     # 'First Name', validators=[DataRequired(), Length(min=2, max=15)])
@@ -40,5 +24,4 @@ class SignupForm(FlaskForm):
     confirm = PasswordField('Confirm Password', validators=[DataRequired(
         message='please confirm password'),
         EqualTo('password', message='Both password fields must be equal!')])
->>>>>>> 84109d3c4398770d8d53c3d9f93dcc300b7326e1
     create = SubmitField('Create Account')
