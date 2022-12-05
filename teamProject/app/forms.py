cec69b6195e66025bcd78febe1b5082d0664a52d
Nick Faylor <nicholas.faylor@sjsu.edu>
2022-12-05 00:08:33 -0800

    added email/username validation

--- teamProject/app/forms.py
+++ teamProject/app/forms.py
@@ -25,3 +25,13 @@ class SignupForm(FlaskForm):
         message='please confirm password'),
         EqualTo('password', message='Both password fields must be equal!')])
     create = SubmitField('Create Account')
+
+    def validate_email(self, email):
+        user_object = User.query.filter_by(email=email.data).first()
+        if user_object:
+            raise ValidationError("Email already taken. Please pick a different Email address.")
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already exists. Please pick a different username.")
