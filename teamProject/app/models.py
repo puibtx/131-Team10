469509954eef734877ca90c6a6336fa57c0738a0
Jose Cortes <67614349+torta19@users.noreply.github.com>
2022-12-06 15:19:54 -0800

    cleaned up files to merge search tool with jose-remote

--- teamProject/app/models.py
+++ teamProject/app/models.py
@@ -3,6 +3,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import login
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), index=True, unique=True)
@@ -10,15 +11,6 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(15), index=True)
     last_name = db.Column(db.String(15), index=True)
     password = db.Column(db.String(100))
-<<<<<<< HEAD
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-# @login.user_loader
-# def load_user(id):
-#     return User.query.get(int(id))
-=======
 
     def get_username(self):
         return self.username
->>>>>>> b439867ab5de07ba0fe9ea7a0ad1d993007f78c6
