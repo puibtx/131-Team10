a6ffe951255de8d2b71359c00b260f856e1ed3c1
puibtx <puibtx@gmail.com>
2022-12-07 21:19:36 -0800

    reset

---
+++ app/models.py
@@ -0,0 +1,57 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    data = db.Column(db.String(250))
+    date = db.Column(db.DateTime(timezone=True), default = func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    username = db.Column(db.String(15), index=True, unique=True)
+    first_name = db.Column(db.String(15), index=True)
+    last_name = db.Column(db.String(15), index=True)
+    password = db.Column(db.String(100))
+    posts = db.relationship('Post')
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    # def follower_count(self)
+    #     return
+
+    # def following_count(self)
+
+    #post of the people you followed
+    # def followed_posts(self):
+    #     return Post.query.join(
+    #         followers, (followers.c.followed_id == Post.user_id)).filter(
+    #             followers.c.follower_id == self.id).order_by(
+    #                 Post.timestamp.desc())
+
+    def get_username(self):
+        return self.username
---
+++ app/routes.py
@@ -0,0 +1,99 @@
+from flask import Blueprint, render_template, flash, redirect, url_for, request, jsonify
+from flask_login import login_required, current_user
+
+from .models import User, Post
+from . import db
+import json
+
+views = Blueprint('routes', __name__)
+
+
+@views.route('/delete')
+@login_required
+def delete():
+
+    delete_user = User.query.filter_by(id=current_user.id).first()
+
+    try:
+
+        db.session.delete(delete_user)
+        db.session.commit()
+        flash('account deleted')
+        return redirect(url_for('auth.login'))
+
+    except:
+        flash('failed to delete account')
+
+
+@views.route('/dashboard/<username>/post', methods=['GET', 'POST'])
+@login_required
+def post(username):
+    if request.method == 'POST':
+        post = request.form.get('post')
+
+        if len(post) > 250:
+            flash('Text up to 250 characters', category='error')
+        else:
+            new_post = Post(data=post, user_id=current_user.id)
+            db.session.add(new_post)
+            db.session.commit()
+            flash('Post uploaded', category='success')
+            return redirect(url_for('routes.home'))
+
+    return render_template('home.html', user=current_user)
+
+
+@views.route('/delete-post', methods=['POST'])
+@login_required
+def deletePost():
+    post = json.loads(request.data)
+    postId = post['postId']
+    post = Post.query.get(postId)
+    if post:
+        if post.user_id == current_user.id:
+            db.session.delete(post)
+            db.session.commit()
+
+    return jsonify({})
+
+
+@views.route('/feed/<username>', methods=['GET', 'POST'])
+@login_required
+def feed(username):
+
+    return render_template('feed.html', username=username)
+
+
+@views.route('/home/<username>', methods=['GET', 'POST'])
+@login_required
+def home(username):
+    user = User.query.filter_by(username=username).first()
+    return render_template('home.html', user=user)
+
+
+@views.route("/search", methods=['GET', 'POST'])
+@login_required
+def search():
+    if request.method == 'POST':
+        username = request.form.get('searched')
+        searched_user = User.query.filter_by(username=username).first()
+
+        if searched_user:
+
+            return redirect(url_for('routes.home', username=username))
+        else:
+            flash('user does not exist.', category='error')
+
+
+@views.route("/home/<username>/following")
+@login_required
+def showfollowing(username):
+    user = User.query.filter_by(username=username).first()
+    return render_template('following.html',users=user.following())
+
+
+# @views.route("/home/<username>/followed")
+# @login_required
+# def search():
+#     if request.method == 'POST':
+        
